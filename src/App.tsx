4e194ec8ab98348e1c0d70e72a24ab34fca86576
Malinda Kumarasinghe <malindakpt@gmail.com>
2021-05-01 06:22:19 +0530

    working

--- src/App.tsx
+++ src/App.tsx
@@ -14,14 +14,81 @@ function App() {
   // const [todoList, setTodoList] = useState<any>();
 
   const loadWeb3 = async () => {
-    web3 = new Web3(Web3.givenProvider || "ws://localhost:8546");
-    // await web3.ethereum.enable()
-  }
+    if (win.web3 !== undefined) {
+      win.web3 = new Web3(win.web3.currentProvider);
+      console.log('Web3 OK')
+    } else {
+      console.log('Not fount')
+    }
+
+    const accounts = await win.ethereum.request({ method: 'eth_requestAccounts' });
+    console.log('account', accounts[0]);
+
+    win.web3.eth.defaultAccount = accounts[0];
+
+    // console.log('default account', win.web3.eth.defaultAccount);
+    // 0xE741731a0aaf841c031786Cac7A169e9896C9168
+    // 0xE741731a0aaf841c031786Cac7A169e9896C9168
+    const contractAddress = '0x936Dc62aC27Da08D3EAca56995Dcd43705CF6238';
+    const contractAbi = [
+      {
+        "inputs": [
+          {
+            "internalType": "uint256",
+            "name": "num",
+            "type": "uint256"
+          }
+        ],
+        "name": "store",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "inputs": [],
+        "name": "retrieve",
+        "outputs": [
+          {
+            "internalType": "uint256",
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      }
+    ];
+
+    // const checksomeaddr = win.web3.utils.toChecksumAddress('0x188bBa4d635317E7530B52f628cb1a6cC33bA7B4')
+
+    //0x188bBa4d635317E7530B52f628cb1a6cC33bA7B4
+    const contract = new win.web3.eth.Contract(contractAbi, contractAddress);
+    // await contract.methods.store(550).send({from :'0xC3248557599d01c9029afAaeba3268f20dEebd08'});
+    //  await contract.methods.retrieve().call();
+
+    try{
+      await contract.methods.store(550).send({from :'0x38b84f33B8646305473FdCe422ef6Bb53D9350af'});
+      // await contract.methods.store(22).call();
+      console.log('Done');
+    } catch(e){
+      console.log(e)
+    }
+    //  const name = await contract.methods.retrieve().call();
+    // contract.methods.retrieve().send({
+    //   from : '0x38b84f33B8646305473FdCe422ef6Bb53D9350af',
+    //   to: '0xE741731a0aaf841c031786Cac7A169e9896C9168'
+    // }).on('recei', (re: any) => {
+    //   console.log('res', re)
+    // })
  
+
+    // console.log(name);
+  }
+
 
   const loadAccount = async () => {
     const accounts = await web3.eth.getAccounts();
-    const acc= accounts[0];
+    const acc = accounts[0];
     web3.eth.defaultAccount = acc;
     console.log('Acc', acc);
   }
@@ -32,16 +99,16 @@ function App() {
     // const contTodo = TruffleContract(todoListJson);
     // contTodo.setProvider(Web3.givenProvider)
 
-    
 
-     TodoList = TruffleContract(todoListJson)
-     TodoList.setProvider(Web3.givenProvider)
+
+    TodoList = TruffleContract(todoListJson)
+    TodoList.setProvider(Web3.givenProvider)
 
     // Hydrate the smart contract with values from the blockchain
-     deployedTodoList = await TodoList.deployed();
- 
-   
-    
+    deployedTodoList = await TodoList.deployed();
+
+
+
     // console.log('todos', deployedTodoList);
   }
 
@@ -55,24 +122,24 @@ function App() {
 
   const printResults = async () => {
     const task = await deployedTodoList.tasks(2);
-    const taskCount: BigInt =  await deployedTodoList.taskCount();
+    const taskCount: BigInt = await deployedTodoList.taskCount();
     // await TodoList.createTask('New from FE');
     console.log('Task', task);
-    console.log('Count:',taskCount.toString());
+    console.log('Count:', taskCount.toString());
   }
 
   const init = async () => {
     await loadWeb3();
-    await loadAccount();
-    await loadContract();
-    await printResults();
+    // await loadAccount();
+    // await loadContract();
+    // await printResults();
   }
 
   useEffect(() => {
     init();
   }, []);
 
-  
+
 
   return (
     <div className="App">
@@ -80,7 +147,7 @@ function App() {
       <div>
         {/* Task Count: {taskCount.toNumber()} */}
       </div>
-      <button onClick={printResults}>Print</button>
+      <button onClick={loadWeb3}>loadWeb3</button>
       <button onClick={addTask}>Add Task</button>
       <button onClick={addTask2}>Add Task 2</button>
     </div>
